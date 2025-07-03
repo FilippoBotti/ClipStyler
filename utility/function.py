fa7ef8bcf7e4dc7b5b882972a88cc62ca03e5c51
teresacal739 <teresa.cal739@gmail.com>
2025-07-03 16:24:57 +0200

    utility functions

--- utility/function.py
+++ utility/function.py
@@ -1,5 +1,9 @@
 import torch
-
+import clip
+import math
+from utility.template import imagenet_templates
+from torchmetrics.image import StructuralSimilarityIndexMeasure
+import torch.nn.functional as F
 
 def calc_mean_std(feat, eps=1e-5):
     # eps is a small value added to the variance to avoid divide-by-zero.
@@ -71,3 +75,47 @@ def coral(source, target):
                         target_f_mean.expand_as(source_f_norm)
 
     return source_f_transfer.view(source.size())
+
+
+def truncate(f):
+    n=3
+    return math.trunc(f * 10**n) / 10**n
+
+def compose_text_with_templates(text: str, templates=imagenet_templates) -> list:
+    return [template.format(text) for template in templates]
+
+def clip_normalize(image, device):
+    image = F.interpolate(image,size=224,mode='bicubic')
+    mean=torch.tensor([0.48145466, 0.4578275, 0.40821073]).to(device)
+    std=torch.tensor([0.26862954, 0.26130258, 0.27577711]).to(device)
+    mean = mean.view(1,-1,1,1)
+    std = std.view(1,-1,1,1)
+
+    image = (image-mean)/std
+    return image
+
+def get_clip_score(clip_model, img, text, device):
+    #out1, out2 = clip_model.encode(clip_normalize(img), clip.tokenize(args.text).to(device))
+    img_feature = clip_model.encode_image(clip_normalize(img, device))
+    tx_feature = clip_model.encode_text(clip.tokenize(text).to(device))
+    img_feature = img_feature / img_feature.norm(dim=1, keepdim=True)
+    tx_feature = tx_feature / tx_feature.norm(dim=1, keepdim=True)
+    score = img_feature @ tx_feature.t()
+    
+    score = score.mean()
+    #print("clip score " + str(score.item()))
+    return score
+
+def get_ssim(preds, target, device):
+    ssim = StructuralSimilarityIndexMeasure().to(device)
+    score = ssim(preds, target)
+    #print("SSIM " + str(score.item()))
+    return score
+
+def get_aesthetic_score(clip_model, model_ae, img, device):
+    img_emb = clip_model.encode_image(clip_normalize(img, device))
+    score = model_ae(img_emb)
+    score = score.mean()
+    #print("Aesthetic score " + str(score.item()))
+    return score
+
